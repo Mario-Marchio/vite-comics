9faa085b17c1d11f07b434485e6c5a429cdc3b3e
Mario-Marchio <mariomarchio1610@gmail.com>
2023-06-29 16:37:37 +0200

    nav done

--- src/App.vue
+++ src/App.vue
@@ -67,8 +67,43 @@ export default {
     <div class="h-30 bgc-black"></div>
     <div class="container header-nav">
       <figure>
-        <img src="./assets/img/dc-logo.png" alt="dc-logo">
+        <img class="p-10" src="./assets/img/dc-logo.png" alt="dc-logo">
       </figure>
+      <div class="r-header-nav">
+        <ul>
+          <li>
+            <a class="color-black px-55 py-5 " href="#">CHARATCTERS</a>
+          </li>
+          <li>
+            <a class="color-black px-55 py-5" href="#">COMICS</a>
+          </li>
+          <li>
+            <a class="color-black px-55 py-5" href="#">MOVIES</a>
+          </li>
+          <li>
+            <a class="color-black px-55 py-5" href="#">TV</a>
+          </li>
+          <li>
+            <a class="color-black px-55 py-5" href="#">GAMES</a>
+          </li>
+          <li>
+            <a class="color-black px-55 py-5" href="#">COLLECTIBLES</a>
+          </li>
+          <li>
+            <a class="color-black px-55 py-5" href="#">VIDEOS</a>
+          </li>
+          <li>
+            <a class="color-black px-55 py-5" href="#">FANS</a>
+          </li>
+          <li>
+            <a class="color-black px-55 py-5" href="#">NEWS</a>
+          </li>
+          <li>
+
+            <a class="color-black px-55 py-5" href="#">SHOP</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </header>
   <main>
@@ -90,6 +125,18 @@ img {
   max-width: 100%;
 }
 
+li {
+  list-style-type: none;
+}
+
+ul {
+  display: flex;
+}
+
+a {
+  text-decoration: none;
+}
+
 .container {
   margin: 0 auto;
   max-width: 1200px;
@@ -99,12 +146,36 @@ img {
   height: 30px;
 }
 
+.p-10 {
+  padding: 10px;
+}
+
+.px-55 {
+  padding-top: 55px;
+  padding-bottom: 55px;
+}
+
+.py-5 {
+  padding-left: 5px;
+  padding-right: 5px;
+}
+
 .bgc-black {
   background-color: black;
+}
+
+.color-black {
+  color: black;
 }
 
 .header-nav {
   display: flex;
   justify-content: space-between;
+}
+
+.r-header-nav {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 </style>
